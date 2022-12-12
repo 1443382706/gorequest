8642c3bfede6311a1f94bf959e57ae43e400119e
你们在哪里啊 <1443382706@qq.com>
2022-12-12 14:31:23 +0800

    添加 request 通用包

---
+++ request/request.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+func GetMore(filter map[string]string, request *http.Request) map[string]string{
+	params := make(map[string]string)
+	request.ParseForm()
+	for k, v := range filter {
+		value, flag := request.Form[k]
+		if flag {
+			params[k] = value[0]
+		}else{
+			params[k] = v
+		}
+	}
+
+	return params
+}
